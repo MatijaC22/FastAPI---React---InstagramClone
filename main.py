14143852083b7af8945204d85b35915969617068
Matija Corak <cmatija2@gmail.com>
2023-04-21 17:34:43 +0530

    next step comments

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from db import models
+from db.database import engine
+from routers import user, post
+from fastapi.staticfiles import StaticFiles
+from auth import authentication
+
+
+import sys
+print(sys.executable)
+
+app = FastAPI()
+app.include_router(user.router)
+app.include_router(post.router)
+app.include_router(authentication.router)
+
+@app.get('/')
+def root():
+    return 'hello world'
+
+models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
